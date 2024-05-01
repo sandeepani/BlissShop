9250806eb67e9a97157f01cc7f373fd79c0b5f33
Sandeepani <h.sandeepani@gmail.com>
2024-05-01 15:11:04 +0530

    2:24

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
