91ca6b9c7a671f4e705ee91a9900211be6c34165
Sandeepani <h.sandeepani@gmail.com>
2024-05-01 19:35:41 +0530

    2:28

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+
+  allowClick = false;
+
+  ngOnInit(): void {
+
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowClick = true;
+    }, 2000);
+  }
 
 }
